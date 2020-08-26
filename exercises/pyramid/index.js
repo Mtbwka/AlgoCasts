e9398210aa24eec6d964d7b64d1118172de3c699
Viktor Kravchuk <viktor.kravchuk@buffl.be>
2020-08-26 09:47:39 +0300

    Pyramid recursion

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,21 +14,41 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {
+// SOLUTION 1
+// function pyramid(n) {
+//   const width = n * 2 - 1;
+//   const center = Math.floor(width / 2);
+
+//   for (let row = 0; row < n; row++) {
+//     let level = '';
+//     for (let col = 0; col < width; col++) {
+//       if (center - row <= col && center + row >= col) {
+//         level += '#';
+//       } else {
+//         level += ' ';
+//       }
+//     }
+//     console.log(level);
+//   }
+// }
+
+// SOLUTION 2
+function pyramid(n, row = 0, col = 0, level = '') {
+  if (n === row) {
+    return;
+  }
   const width = n * 2 - 1;
+
+  if (width === level.length) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
   const center = Math.floor(width / 2);
 
-  for (let row = 0; row < n; row++) {
-    let level = '';
-    for (let col = 0; col < width; col++) {
-      if (center - row <= col && center + row >= col) {
-        level += '#';
-      } else {
-        level += ' ';
-      }
-    }
-    console.log(level);
-  }
+  const add = center - row <= col && center + row >= col ? '#' : ' ';
+
+  return pyramid(n, row, col + 1, level + add);
 }
 
 module.exports = pyramid;
